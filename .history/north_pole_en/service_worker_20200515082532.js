c4cc7e9d325611bd6e4eb4d7082a4c4c99c5e00f
Ran Levi <ran.levi@gmail.com>
2020-05-22 08:38:27 +0300

    Ver 1.08

---
+++ .history/north_pole_en/service_worker_20200515082532.js
@@ -0,0 +1,36 @@
+//Version 002
+
+// 1. Save the files to the user's device
+// The "install" event is called when the ServiceWorker starts up.
+// All ServiceWorker code must be inside events.
+self.addEventListener('install', function(e) {
+  console.log('install');
+  
+  // waitUntil tells the browser that the install event is not finished until we have
+  // cached all of our files
+  e.waitUntil(
+    // Here we call our cache "myonsenuipwa", but you can name it anything unique
+    caches.open('grandpa_benny_app').then(cache => {
+      // If the request for any of these resources fails, _none_ of the resources will be
+      // added to the cache.
+      return cache.addAll([
+        './',
+        './index.html',
+        `./manifest_north_pole_en.json`,        
+        './js/script.js',
+        './css/style.css'
+      ]);
+    })
+  );
+});
+  
+
+// Cache first, else network.
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    // check if this file exists in the cache
+    caches.match(e.request)
+      // Return the cached file, or else try to get it from the server
+      .then(response => response || fetch(e.request))
+  );
+});
